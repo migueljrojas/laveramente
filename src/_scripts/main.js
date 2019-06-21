61f79104ae3ad7ed18e027627d41b16181a37f0d
Mike <migueljrojas@gmail.com>
2019-06-21 12:53:09 -0300

    Added jquery and slcik ast CDN

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,13 +3,13 @@
 
 'use strict';
 
-global.$ = global.jQuery = require('jquery');
-global._ = require('underscore');
+global.$ = global.jQuery;
+//global._ = require('underscore');
+
 var Header = require('../_modules/header/header');
 var Slider = require('../_modules/slider/slider');
 
 $(function() {
-    require('../../bower_components/slick-carousel/slick/slick');
 
     new Header();
     new Slider();
